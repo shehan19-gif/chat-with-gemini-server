7ec6f5d329bb505a6bae3c60343b9d8b0de19271
shehan19-gif <shehanmaduwantha84@gmail.com>
2025-08-28 14:00:26 +0530

    cors changed

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const app = express();
 
 // cors setup
-const allowedOrigins = ["https://chat-free-with-gemini.netlify.app/", "http://localhost:5173"];
+/* const allowedOrigins = ["http://localhost:5173", "https://chat-free-with-gemini.netlify.app/"];
 const corsOptions = {
     origin: function(origin, callback) {
         if(allowedOrigins.indexOf(origin) !== -1) {
@@ -18,10 +18,10 @@ const corsOptions = {
         }
     },
     credentials: true
-};
+}; */
 
 // middlewares
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
